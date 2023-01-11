761cfc381092842161579417e1c7b54414b1830e
PavelZubarev <213633@inbox.ru>
2023-01-11 17:14:22 +0300

    down

--- page_img.php
+++ page_img.php
@@ -1,13 +1,59 @@
+<?php
+session_start();
+  ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="styles.css">
+  
     <title>Document</title>
 </head>
-<body>
-    
+<body class="use_img">
+ 
+ <section class="box_img">
+<?php  
+ $url = $_SERVER['REQUEST_URI'];
+ $url = explode('/', $url);
+
+$img_del=str_img($url[2]);
+
+  function str_img($img){
+  echo '<div class="box_img_style">';
+  echo '<img class="page_img_style" src="/photo/'. $_SESSION['login'] . '/' . $img . '"alt=""> ';
+  echo '<form action="/phpcode/del.php" method="post">';
+  echo'<input class="ninja" type="text" name="del" value ="'. $img .'" >';
+  if (isset($img)) {
+    echo'<input type="submit" class="del_but" value="Удалить">';
+}
+echo '</form>' ;
+}
+
+
+if ($url[2]===$img_del) {
+  header('Location: /personal_account.php');
+}
+?>
+</div>
+</section>
 </body>
 </html>
+
+
+
+
+
+
+
+
+
+<!-- 
+echo '<pre>';
+echo $_SESSION['login'];
+// echo print_r( $_SESSION['login']);
+// echo print_r($url);
+echo '</pre>';
+
+    -->
--- personal_account.php
+++ personal_account.php
@@ -11,6 +11,7 @@ if (!$_SESSION['name']) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+
     <title>Document</title>
 </head>
 <body class="user">
@@ -21,6 +22,7 @@ if (!$_SESSION['name']) {
     <form class="down_fi" action="phpcode/down_f.php" enctype="multipart/form-data" method="post">
     <li class="file_lable" ><label  for="down_file">Добавить</label></li>
      <input id="down_file" class="file_push" type="file" name="down_file" >
+    
    
     <li><input type="submit" class="down_sub" value="отправит"></li>
     </form> 
@@ -28,6 +30,7 @@ if (!$_SESSION['name']) {
     
 </header>
 <section class="img_sec">
+
 <?php
 
 if ($_SESSION['name_img']!='') {
@@ -35,19 +38,25 @@ if ($_SESSION['name_img']!='') {
   //////////////////////////////////////////////////////////////////////// пагенация
   $p = isset($_GET["p"]) ? (int) $_GET["p"] : 1;
   $str_img=8;
+  $str_visual=4;
 
-//  echo '<pre>';
-//  echo print_r($_SESSION["name_img"]);
-//  echo '</pre>';
+
  $arr_rev=array_merge(array_reverse($_SESSION['name_img']));
   
   $str_start=($p-1)*$str_img;
 $str_page=ceil(count($_SESSION['name_img'])/$str_img);
+$str_page_ostatoc= abs(ceil(count($_SESSION['name_img'])-($str_start)));
+if($str_page_ostatoc>8){$str_page_ostatoc=8;}
 
-
-    for ($i=$str_start; $i <$str_start+$str_img ; $i++) { 
+// echo '<pre>';
+// // echo print_r($_SESSION["name_img"]);
+// echo $str_page_ostatoc;
+// echo '</pre>';
+// for ($i=$str_start; $i <$str_start+$str_img; $i++) { 
+    for ($i=$str_start; $i <$str_start+$str_page_ostatoc; $i++) { 
+      
       echo '<div class="box">';
-      echo '<img class="img_sec_user" src="photo/'. $_SESSION['login'] . '/' .$arr_rev[$i] . '"alt=""> ';
+      echo '  <a href="/page_img.php/'.$arr_rev[$i] .'"><img class="img_sec_user" src="photo/'. $_SESSION['login'] . '/' .$arr_rev[$i] . '"alt=""> </a>';
       echo '<form action="phpcode/del.php" method="post">';
       echo'<input class="ninja" type="text" name="del" value ="'. $arr_rev[$i] .'" >';
       if (isset($arr_rev[$i])) {
@@ -62,18 +71,7 @@ $str_page=ceil(count($_SESSION['name_img'])/$str_img);
 
 
 
-// foreach (array_reverse($_SESSION['name_img']) as $key => $value_img) {
-//   echo '<div class="box">';
-//   // echo '<a href="page_img.php"><img class="img_sec_user" src="photo/'. $_SESSION['login'] . '/' . $value_img . '"alt=""></a> ';
-//   echo '<img class="img_sec_user" src="photo/'. $_SESSION['login'] . '/' . $value_img . '"alt=""> ';
-//   echo '<form action="phpcode/del.php" method="post">';
-//   echo'<input class="ninja" type="text" name="del" value ="'. $value_img .'" >';
-//   if (isset($value_img)) {
-//     echo'<input type="submit" class="del_but" value="Удалить">';
-// }
-// echo '</form>' ;
-// echo '</div>';
-// }
+
 
 
 
@@ -87,6 +85,7 @@ $str_page=ceil(count($_SESSION['name_img'])/$str_img);
 <? for($i = 1; $i <= $str_page; $i++){ ?>
 <a href="?p=<?= $i ?>"><?= $i?></a>
 <? } ?>
+
 
 
 </section>
