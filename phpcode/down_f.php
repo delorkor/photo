7cf066767c64df99e54e8ead46e31bfd4a8df429
PavelZubarev <213633@inbox.ru>
2023-01-15 21:04:38 +0300

    down

--- phpcode/down_f.php
+++ phpcode/down_f.php
@@ -1,50 +1,5 @@
 <?php
 session_start();
-////////////////////////////////////////запись файла
-// if(!is_dir('../photo/'. $_SESSION['login'])){
-// mkdir('../photo/'. $_SESSION['login']);
-
-// $path= "photo/". $_SESSION['login'] . "/" . time() . $_FILES['down_file']['name'];
-//     move_uploaded_file( $_FILES['down_file']['tmp_name'],'../'. $path);
-
-// header('Location: ../personal_account.php');
-
-// }
-// else {
-//     $path= "photo/". $_SESSION['login'] . "/" . time() . $_FILES['down_file']['name'];
-//     move_uploaded_file( $_FILES['down_file']['tmp_name'],'../'. $path);
-//     header('Location: ../personal_account.php');
-// }
-// ////////////////////////////////////////////////
-
-
-
- 
-// //////////////////////////////// чтение файла
-
-// $dir_img = opendir('../photo/'. $_SESSION['login']);
-// down_file($dir_img);
-// closedir($dir_img);
-// //////////////////////////////////////////
-
-
-
-
-// function down_file($dir_i){
-//     $_SESSION['name_img']=[];
-//     // $arr_dir_file =[];
-//     while (false !== ($entry = readdir($dir_i))) {
-//         if ($entry != "." && $entry != "..") {
-//             $_SESSION['name_img'][] =$entry;
-    
-//             // echo '<pre>';
-//             // // echo print_r($arr_dir_file);
-//             // echo print_r( $_SESSION['name_img']);
-//             // echo '</pre>';
-//         }
-//     } 
-// }
-
 class down_file
 {
 public $name_img=[];
@@ -79,13 +34,14 @@ else {
   }
 
   function DelFile(){
-    $dir_img = opendir('../photo/'. $_SESSION['login']);
+    // $dir_img = opendir('../photo/'. $_SESSION['login']);
     foreach ($_SESSION['name_img'] as $key=> $value_del) {
         if ($_POST['del']== $value_del) {
             unlink('../photo/'. $_SESSION['login'] . "/" . $value_del);
         }
     }
     header('Location: ../personal_account.php');
+    // closedir($dir_img);
   }
 
 
@@ -94,6 +50,5 @@ else {
 
 $Down_f = new down_file();
 $Down_f->setFile();
-// $Down_f->getFile();
 $Down_f->DelFile();
 $Down_f->getFile();
